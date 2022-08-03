105e604998ded1ddb792f5f672977effecb09e72
lidorkalfa <lidorkalfa@gmail.com>
2022-08-03 22:23:33 +0300

    started a new project

--- Studying/Javascript/JS - obj-fun-01.08.2022/2/js/main.js
+++ Studying/Javascript/JS - obj-fun-01.08.2022/2/js/main.js
@@ -1,13 +1,15 @@
 const myTriangle = {
 
-    sqr : function (num) {
-        return num**2
+    sqr: function (num) {
+        return num ** 2;
     },
 
     sum: function(num1, num2){
-        return num1+num2
+        return num1+num2;
     },
 
-    isRightAngle:function(){}
+    isRightAngle:function(num1, num2, num3){
+        return this.sum(this.sqr(num1), this.sqr(num2) === this.sqr(num3))
+    },
 
-}
+};
